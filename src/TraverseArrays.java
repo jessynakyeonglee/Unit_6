b211896ef2f10ebfbc6a232c5a06eab3b7e8f7db
leena <leena@ssfs.org>
2020-01-10 12:05:27 -0500

    1/10/20 E Block

--- src/TraverseArrays.java
+++ src/TraverseArrays.java
@@ -1,20 +1,21 @@
 public class TraverseArrays {
 
     public static int countEven(int[] nums){
-        int evens=0;
-        int count=0;
-        while (count<=nums.length){
+        int evens = 0;
+        for (int count=0;count<nums.length;count++){
             if (nums[count]%2==0){
                 evens++;
-                count++;
-            }
-            else{
-                count++;
             }
         }
         return evens;
     }
+    public static int[] buildArray(int desiredLength){
+        int []array={};
+        for (int count=0;count<desiredLength;count++){
+            array+=count;
+        }
+    }
     public static void main (String[]args){
-        System.out.print(countEven( [2, 1,
+        System.out.print(countEven(new int[] {2,1,2,3,4}));
     }
 }
